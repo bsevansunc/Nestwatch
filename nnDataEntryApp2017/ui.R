5360562e834f91583aa7bae3b485e98afe259424
Brian S Evans <bsevans.unc@gmail.com>
2017-03-22 14:16:48 -0400

    update of empty fields to no data or 99999

--- nnDataEntryApp2017/ui.R
+++ nnDataEntryApp2017/ui.R
@@ -15,7 +15,8 @@ shinyUI(
       fluidPage(
         useShinyjs(),
         fluidRow(
-               column(3, selectInput('hub','Regional Hub:', choiceRegions)),
+               column(3, selectInput('hub','Regional Hub:', choiceRegions,
+                                     selected = 'noData')),
                column(3, selectInput('inputType', 'New or existing site:',
                                      c('Existing site', 'New site'))),
                column(4, uiOutput('uiSite')),
@@ -34,17 +35,17 @@ shinyUI(
     h4(strong('Site contact information:')),
     br(),
     fluidRow(
-      column(3, textInput('siteIDContact', 'Site ID')),
-      column(3, textInput('groupNameContact', 'Group name')),
-      column(3, textInput('lastNameContact' ,'Last name')),
-      column(3, textInput('firstNameContact', 'First name'))
+      column(3, textInput('siteIDContact', 'Site ID', 'noData')),
+      column(3, textInput('groupNameContact', 'Group name', 'noData')),
+      column(3, textInput('lastNameContact' ,'Last name', 'noData')),
+      column(3, textInput('firstNameContact', 'First name', 'noData'))
     ),
     br(),
     fluidRow(
-      column(2, textInput('phone1Contact', 'Primary phone')),
-      column(2, textInput('phone2Contact' ,'Secondary phone')),
-      column(3, textInput('email1Contact', 'Email')),
-      column(5, textInput('notesContact', 'Contact notes'))
+      column(2, textInput('phone1Contact', 'Primary phone', 'noData')),
+      column(2, textInput('phone2Contact' ,'Secondary phone', 'noData')),
+      column(3, textInput('email1Contact', 'Email', 'noData')),
+      column(5, textInput('notesContact', 'Contact notes', 'noData'))
     ),
     br(),
     fluidRow(column(6, ''),
@@ -72,16 +73,16 @@ shinyUI(
     h4(strong('Site address information:')),
     br(),
     fluidRow(
-      column(3, textInput('siteIDAddress', 'Site ID')),
-      column(2, textInput('houseNumberAddress', 'House number')),
-      column(2, textInput('streetAddress' ,'Street')),
-      column(2, textInput('cityAddress', 'City')),
-      column(1, textInput('stateAddress', 'State')),
-      column(2, textInput('zipAddress', 'Zip code'))
+      column(3, textInput('siteIDAddress', 'Site ID', 'noData')),
+      column(2, textInput('houseNumberAddress', 'House number', 'noData')),
+      column(2, textInput('streetAddress' ,'Street', 'noData')),
+      column(2, textInput('cityAddress', 'City', 'noData')),
+      column(1, textInput('stateAddress', 'State', 'noData')),
+      column(2, textInput('zipAddress', 'Zip code', 'noData'))
     ),
     br(),
     fluidRow(
-      column(11, textInput('notesAddress', 'Address notes')),
+      column(11, textInput('notesAddress', 'Address notes', 'noData')),
       column(1, '')
     ),
     br(),
@@ -110,18 +111,19 @@ shinyUI(
     h4(strong('Site location information:')),
     br(),
     fluidRow(
-      column(3, textInput('siteIDLocation', 'Site ID')),
-      column(2, dateInput('dateLocation', 'Date')),
-      column(2, numericInput('longLocation' ,'Longitude', NA)),
-      column(2, numericInput('latLocation', 'Latitude', NA)),
-      column(1, numericInput('accuracyLocation', 'Accuracy', NA)),
+      column(3, textInput('siteIDLocation', 'Site ID', 'noData')),
+      column(2, dateInput('dateLocation', 'Date'), 'noData'),
+      column(2, numericInput('longLocation' ,'Longitude', 99999)),
+      column(2, numericInput('latLocation', 'Latitude', 99999)),
+      column(1, numericInput('accuracyLocation', 'Accuracy', 99999)),
       column(2, selectizeInput('locationMethodLocation',
                                'Location method', 
-                               choices = choiceLocationMethod))
+                               choices = choiceLocationMethod,
+                               selected = 'noData'))
     ),
     br(),
     fluidRow(
-      column(12, textInput('notesLocation', 'Location notes'))
+      column(12, textInput('notesLocation', 'Location notes', 'noData'))
     ),
     br(),
     fluidRow(column(6, ''),
@@ -149,16 +151,18 @@ shinyUI(
     h4(strong('Site visit information:')),
     br(),
     fluidRow(
-      column(3, textInput('siteIDVisit', 'Site ID')),
-      column(2, dateInput('dateVisit', 'Date')),
-      column(2, textInput('observerVisit' ,'Observer(s)')),
+      column(3, textInput('siteIDVisit', 'Site ID', 'noData')),
+      column(2, dateInput('dateVisit', 'Date', '2017-01-01')),
+      column(2, textInput('observerVisit' ,'Observer(s)', 'noData')),
       column(5, selectizeInput('participantEngagementVisit',
                                'Participant Engagement',
-                               choices = choiceParticipantEngagement))
+                               choices = choiceParticipantEngagement,
+                               selected = 'noData'))
     ),
     fluidRow(
       column(2, selectizeInput('encounteredBirdsVisit', 'Encountered birds',
-                               choices = choiceEncounteredBirds)),
+                               choices = choiceEncounteredBirds,
+                               selected = 'noData')),
       column(2, numericInput('netMinutes6Visit', 
                              '6 m nets', 
                              0)),
@@ -170,11 +174,12 @@ shinyUI(
                              0)),
       column(2, numericInput('netMinutes18Visit', 
                              '18 m nets', 
-                             0))
+                             0)),
+      column(2, numericInput('netHoursVisit', 'Net hours', 0))
     ),
     br(),
     fluidRow(
-      column(12, textInput('notesVisit', 'Visit notes'))
+      column(12, textInput('notesVisit', 'Visit notes', 'noData'))
     ),
     br(),
     fluidRow(column(6, ''),
@@ -199,38 +204,47 @@ shinyUI(
     #--------------------------------------------------------------------*
     # ---- Capture table ----
     #--------------------------------------------------------------------*
+    h4(strong('Capture information:')),
     br(),
     fluidRow(
-      column(3, textInput('siteIDCapture', 'Site ID')),
-      column(3, textInput('dateCapture', 'Date')),
+      column(3, textInput('siteIDCapture', 'Site ID', 'noData')),
+      column(3, textInput('dateCapture', 'Date', 'noData')),
       column(6, '')
     ),
     fluidRow(
       column(2, selectizeInput('timeCapture', 'Time',
-                               choices = choiceTimeOfDay)),
-      column(1, textInput('observerCapture', 'Observer')),
+                               choices = choiceTimeOfDay,
+                               selected = '00:01')),
+      column(1, textInput('observerCapture', 'Observer', 'noData')),
       column(1, selectizeInput('encCapture', 'Encounter',
-                               choices = choiceEnc)),
+                               choices = choiceEnc,
+                               selected = 'noData')),
       column(2, selectizeInput('sppCapture', 'SPP',
-                               choices = choiceSpecies)),
-      column(3, textInput('bandNumberCapture', 'Band number')),
+                               choices = choiceSpecies,
+                               selected = 'noData')),
+      column(3, textInput('bandNumberCapture', 'Band number', 'noData')),
       column(3, selectizeInput('colorComboCapture', 'Color combo',
-                               choices = choiceColors))
+                               choices = choiceColors,
+                               selected = 'noData'))
     ),
     fluidRow(
-      column(2, numericInput('massCapture', 'Mass', NA)),
-      column(2, numericInput('wingCapture', 'Wing', NA)),
-      column(2, numericInput('tlCapture', 'Tail', NA)),
-      column(2, selectizeInput('ageCapture', 'Age', choices = choiceAge)),
-      column(1, selectizeInput('sexCapture', 'Sex', choices = choiceSex)),
+      column(2, numericInput('massCapture', 'Mass', 99999)),
+      column(2, numericInput('wingCapture', 'Wing', 99999)),
+      column(2, numericInput('tlCapture', 'Tail', 99999)),
+      column(2, selectizeInput('ageCapture', 'Age', choices = choiceAge,
+                               selected = 'noData')),
+      column(1, selectizeInput('sexCapture', 'Sex', choices = choiceSex,
+                               selected = 'noData')),
       column(1, selectizeInput('cpBpCapture', 'CP/BP',
-                               choices = choiceBreedingCond)),
-      column(1, selectizeInput('fatCapture', 'Fat', choices = choiceFat))
+                               choices = choiceBreedingCond,
+                               selected = 'noData')),
+      column(1, selectizeInput('fatCapture', 'Fat', choices = choiceFat,
+                               selected = 'noData'))
     ),
     fluidRow(
-      column(2, textInput('bloodIDCapture', 'Blood sample ID')),
-      column(2, textInput('featherIDCapture', 'Feather sample ID')),
-      column(8, textInput('notesCapture', 'Capture notes'))
+      column(2, textInput('bloodIDCapture', 'Blood sample ID', 'noData')),
+      column(2, textInput('featherIDCapture', 'Feather sample ID', 'noData')),
+      column(8, textInput('notesCapture', 'Capture notes', 'noData'))
     ),
     br(),
     fluidRow(column(6, ''),
@@ -255,21 +269,24 @@ shinyUI(
     #--------------------------------------------------------------------*
     # ---- Resight foray effort table ----
     #--------------------------------------------------------------------*
+    h4(strong('Resight foray information:')),
     br(),
     fluidRow(
-      column(3, textInput('siteIDForayEffort', 'Site ID')),
-      column(3, textInput('dateForayEffort', 'Date')),
+      column(3, textInput('siteIDForayEffort', 'Site ID', 'noData')),
+      column(3, textInput('dateForayEffort', 'Date', '2017-01-01')),
       column(6, '')
     ),
     br(),
     fluidRow(
-      column(2, textInput('observerForayEffort', 'Observer')),
+      column(2, textInput('observerForayEffort', 'Observer', 'noData')),
       column(2, numericInput('numberForayEffort', 'Foray number', 1)),
       column(2, selectizeInput('startTimeForayEffort', 'Start time',
-                               choices = choiceTimeOfDay)),
+                               choices = choiceTimeOfDay,
+                               selected = '00:01')),
       column(2, selectizeInput('endTimeForayEffort', 'End time',
-                               choices = choiceTimeOfDay)),
-      column(2, numericInput('pathDistanceForayEffort', 'Path distance (m)', NA)),
+                               choices = choiceTimeOfDay,
+                               selected = '00:01')),
+      column(2, numericInput('pathDistanceForayEffort', 'Path distance (m)', 99999)),
       column(2, '')
     ),
     br(),
@@ -295,12 +312,14 @@ shinyUI(
     #--------------------------------------------------------------------*
     # ---- Resight foray observed unbanded table ----
     #--------------------------------------------------------------------*
+    h4(strong('Unbanded focal birds observed while on foray:')),
     br(),
     fluidRow(
-      column(3, textInput('siteIDForayCountUnbanded', 'Site ID')),
-      column(3, textInput('dateForayCountUnbanded', 'Date')),
+      column(3, textInput('siteIDForayCountUnbanded', 'Site ID', 'noData')),
+      column(3, textInput('dateForayCountUnbanded', 'Date', '2017-01-01')),
       column(2, selectizeInput('sppForayCountUnbanded', 'Species',
-                               choices = choiceSpecies)),
+                               choices = choiceSpecies,
+                               selected = 'noData')),
       column(2, numericInput('countForayCountUnbanded', 'Count', 0)),
       column(2, '')
     ),
@@ -332,25 +351,28 @@ shinyUI(
     #--------------------------------------------------------------------*
     # ---- Resighted birds by technician table ----
     #--------------------------------------------------------------------*
+    h4(strong('Resighted birds during the visit:')),
     br(),
     fluidRow(
-      column(3, textInput('siteIDTechRs', 'Site ID')),
-      column(3, textInput('dateTechRs', 'Date')),
+      column(3, textInput('siteIDTechRs', 'Site ID', 'noData')),
+      column(3, textInput('dateTechRs', 'Date', 'noData')),
       column(6, '')
     ),
     fluidRow(
-      column(1, textInput('observerTechRs', 'Observer')),
+      column(1, textInput('observerTechRs', 'Observer', 'noData')),
       column(2, selectizeInput('timeTechRs', 'Time',
-                               choices = choiceTimeOfDay)),
-      column(2, numericInput('forayNumberTechRs', 'Foray number', NA)),
-      column(2, textInput('bandNumberTechRs', 'Band number')),
-      column(2, numericInput('longTechRs', 'Longitude (-dd.dddd)', NA)),
-      column(2, numericInput('latTechRs', 'Latitude (dd.dddd)', NA)),
+                               choices = choiceTimeOfDay,
+                               selected = '00:01')),
+      column(2, textInput('forayNumberTechRs', 'Foray number', 'noData')),
+      column(2, textInput('bandNumberTechRs', 'Band number', 'noData')),
+      column(2, numericInput('longTechRs', 'Longitude (-dd.dddd)', 99999)),
+      column(2, numericInput('latTechRs', 'Latitude (dd.dddd)', 99999)),
       column(1, selectizeInput('typeTechRs', 'Resight type',
-                               choices = choiceTypeRs))
+                               choices = choiceTypeRs,
+                               selected = 'noData'))
     ),
     fluidRow(
-      column(12, textInput('notesTechRs', 'Notes'))
+      column(12, textInput('notesTechRs', 'Notes', 'noData'))
       ),
     br(),
     fluidRow(column(6, ''),
@@ -385,28 +407,32 @@ shinyUI(
     textAouQuery,
     fluidRow(column(11, DT::dataTableOutput('aouTable'))),
     hr(),
+    h4(strong('Point count information:')),
     br(),
     fluidRow(
-      column(3, textInput('siteIDPc', 'Site ID')),
-      column(3, textInput('datePc', 'Date')),
-      column(1, textInput('observerPc', 'Observer')),
+      column(3, textInput('siteIDPc', 'Site ID', 'noData')),
+      column(3, textInput('datePc', 'Date', 'noData')),
+      column(1, textInput('observerPc', 'Observer', 'noData')),
       column(2, selectizeInput('startTimePc', 'Start time',
-                               choices = choiceTimeOfDay)),
-      
+                               choices = choiceTimeOfDay,
+                               selected = '00:01')),
       column(3, '')
     ),
     fluidRow(
       column(1, selectizeInput('intervalPc', 'Interval', 
-                               choices = choiceInterval)),
+                               choices = choiceInterval,
+                               selected = 1)),
       column(2, selectizeInput('sppPc', 'Species',
-                               choices = choiceSpecies)),
+                               choices = choiceSpecies,
+                               selected = 'noData')),
       column(1, numericInput('count10Pc', 'DIST < 10', 0)),
       column(1, numericInput('count20Pc', 'DIST 10 - 20', 0)),
       column(1, numericInput('count30Pc', 'DIST 20 - 30', 0)),
       column(1, numericInput('count40Pc', 'DIST 30 - 40', 0)),
       column(1, numericInput('count50Pc', 'DIST 50 - 50', 0)),
       column(2, selectizeInput('detectionPc', 'Detection',
-                               choices = choiceDetection))
+                               choices = choiceDetection,
+                               selected = 'noData'))
     ),
     br(),
     fluidRow(column(6, ''),
